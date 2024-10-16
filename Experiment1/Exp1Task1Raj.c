d80f5e004df92b0e87cd143e1fe40ba99963c9f6
Raj-MM <rajmaurya23@eng.rizvi.edu.in>
2024-10-16 15:54:48 +0530

    Add files via upload

---
+++ Experiment1/Exp1Task1Raj.c
@@ -0,0 +1,19 @@
+//Aim: WAP to print your name, age, class, division and UIN on the screen.
+//Use escape sequences to properly format the output.
+
+#include<stdio.h>
+int main()
+{
+    printf("Full Name\t:Raj Maurya");
+    printf("\nAge     \t:18");
+    printf("\nClass   \t:FE");
+    printf("\nDivision  \t:A");
+    printf("\nUIN      \t:241A008");
+    return 0;
+}
+/*EXPECTED OUTPUT:
+Full Name       : Raj Maurya
+Age             : 18
+Class           : FE
+Division        : A
+UIN             : 241A008*/
